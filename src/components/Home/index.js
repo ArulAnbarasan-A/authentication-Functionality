6c4499661279c5210c4764e8ed8f209dcf622338
ArulAnbarasan-A <anbu91943@gmail.com>
2024-02-20 04:18:02 +0000

    aunthentication

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import LogoutButton from '../LogoutButton'
+import Header from '../Header'
+import './index.css'
+
+const Home = () => (
+  <>
+    <div className="home">
+      <h1>Home Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+export default Home
